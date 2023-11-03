d345c326ea2241ab61805cd50c3378375bad0264
MuthuJawahar1108 <kvmjawa20031108@gmail.com>
2023-11-03 11:38:42 +0530

    Successfully established connection with mysql and completed the login page. Have to complete the dashboard page!!

---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+    include("connection.php");
+
+    if(isset($_POST['submit'])){
+        $username = $_POST['username'];
+        // $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $sql = "select * from users where name = '$username' and pass = '$password'";
+        $result = mysqli_query($conn,$sql);
+        $row = mysqli_fetch_row($result);
+
+        $count = mysqli_num_rows($result);
+        echo "$count";
+        if($count==1){
+            header("Location: home.html");
+            // echo '<script>window.location.href = "home.html";</script>';
+        } else {
+            echo '<script>
+                alert("Login failed..Invalid username or password!")
+                window.location.href="index.html"
+                </script>';
+        }
+    }
+?>
